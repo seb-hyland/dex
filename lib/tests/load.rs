e6cc2cfe354b0c23481183c05e3b86e75c7ae6f8
Sebastian Hyland <st.hyland05@gmail.com>
2026-02-20 14:07:27 -0800

    feat(lib/load): universal char-delimited file loading

--- lib/tests/load.rs
+++ lib/tests/load.rs
@@ -1,11 +1,8 @@
-use std::fs::File;
-use lib::load::{open_csv, sniff_file};
+use csv_nose::{SampleSize, Sniffer};
+use lib::load::load_delimited_file;
 
 #[test]
 fn test_sniff() {
-    let file = File::open("test2.csv").unwrap();
-    let format = sniff_file(file.try_clone().unwrap()).unwrap();
-    println!("{format:#?}");
-
-    // let _batch = open_csv(file.try_clone().unwrap(), file.try_clone().unwrap(), format).unwrap();
+    let batch = load_delimited_file("test.csv").unwrap();
+    println!("{batch:#?}");
 }
